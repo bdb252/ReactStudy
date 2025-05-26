217a057b6c7446e9b50cacfb22cc0f605fc1022e
bdb252 <hanyilee@naver.com>
2025-05-26 16:48:35 +0900

    0526 storage

---
+++ react15-storage/src/storageConfig.jsx
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
+  databaseURL: import.meta.env.VITE_databaseURL
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// storage 객체 생성(본인의 스토리지 gs주소 사용)
+const storage = getStorage(app, "gs://myreactapp-2731c.firebasestorage.app");
+export {storage}
